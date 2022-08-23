da9d1b8228ebfc8530642fedc6ea13016971c333
Maros <mbenadik25@gmail.com>
2022-08-23 18:09:00 +0100

    heroku update

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const journyRoute = require('./routes/ourJourny');
 const port = process.env.PORT || 8800;
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", " http://localhost:3000"); 
+    //res.header("Access-Control-Allow-Origin", "http://localhost:3000"); 
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
